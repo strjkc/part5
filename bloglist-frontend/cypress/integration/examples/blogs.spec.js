9bed12827c3c95bb423d4e9b82eef55d357128dd
strahinja <strahinjaa.va@gmail.com>
2020-09-06 13:01:33 +0200

    test-user can log in

--- bloglist-frontend/cypress/integration/examples/blogs.spec.js
+++ bloglist-frontend/cypress/integration/examples/blogs.spec.js
@@ -3,6 +3,12 @@ const { idText, createYield } = require("typescript")
 describe('login form', function(){
     beforeEach(function(){
         cy.request('POST', 'http://localhost:3003/api/test/reset')
+        const newUser = {
+          username: 'testUser',
+          name: 'Test User',
+          password: 'testUser'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', newUser)
         cy.visit('http://localhost:3000/')
     })
     it('login form is displayed by default', function() {
@@ -13,5 +19,12 @@ describe('login form', function(){
         .and('contain','Login')
         cy.get('#username-input')
         cy.get('#password-input')
+    })
+
+    it('the user is able to log in', function() {
+      cy.get('#username-input').type('testUser')
+      cy.get('#password-input').type('testUser')
+      cy.contains('Login').click()
+      cy.contains('Test User logged in')
     })
 })
